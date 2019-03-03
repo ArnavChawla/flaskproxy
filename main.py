a418cb9159d80cf98a1950bf9c40b67131fa132d
Arnav Chawla <arnavchawla23@gmail.com>
2019-03-03 12:17:22 -0800

    working

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-
+import requests
+import flask
 from flask import Flask
-from requests import get
+from flask import request
+from flask import Response
+import base64
+app = Flask(__name__)
+from bs4 import BeautifulSoup as soup
 
-app = Flask('__main__')
-SITE_NAME = 'https://reddit.com/'
+@app.route("/")
+def give():
+  url = request.args.get('url')
+  print(url)
+  session = requests.Session()
+  baseUrl = "http://127.0.0.1:5000/?url=http://w3schools.com/"
+  linkUrl = "http://127.0.0.1:5000/link?url=http://w3schools.com/"
+  imgUrl = "http:/127.0.0.1:5000/image?url=http://w3schools.com/"
+  r = session.get(url)
+  r.encoding = "utf-8-sig"
+  bs = soup(r.content, "html.parser")
+  for a in bs.findAll('a'):
+        a['href'] = baseUrl + a['href']
+  for link in bs.findAll('img'):
+      try:
+          link['src'] = imgUrl + "/" +link['src']
+      except:
+          pass
+  for link in bs.findAll('script'):
+      try:
+          link['src'] = baseUrl + "/" +link['src']
+      except:
+          pass
+  for link in bs.findAll('iframe'):
+      try:
+          link['src'] = baseUrl + "/" +link['src']
+      except:
+          pass
+  for link in bs.findAll('link'):
+    link['href'] = linkUrl + "/" +link['href']
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def proxy(path):
-  header={
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-  }
-  return get(f'{SITE_NAME}{path}',headers=header).content
-
-app.run(host='0.0.0.0', port=8080)
+  return " " +str(bs)
+@app.route("/image")
+def img():
+  r = base64.b64encode(requests.get(request.args.get('url')).content)
+  src = "data:image/png;base64," + r
+  return src
+@app.route("/link")
+def pain():
+    r = requests.get(request.args.get("url"))
+    r.encoding = "utf-8-sig"
+    return Response(r.text, mimetype='text/css')
+if __name__ == "__main__":
+  app.run("0.0.0.0",port=5000,debug=True)
